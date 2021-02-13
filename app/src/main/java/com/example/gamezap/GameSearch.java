f9be26f0dfe10022da846d472596cac679191def
Omri Gilhar <gilharomri@gmail.com>
2021-02-13 15:33:30 +0200

    Added all basic layouts for the app/
    * Main Layout
    * SignUp Layout
    * GameSearch Layout
    * GamePage Layout
    * ProfilePage Layout

---
+++ app/src/main/java/com/example/gamezap/GameSearch.java
@@ -0,0 +1,14 @@
+package com.example.gamezap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
