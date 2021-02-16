d1860d1fe27abddca1375b808f72d868e1b3dc87
Omri Gilhar <gilharomri@gmail.com>
2021-02-16 17:28:08 +0200

    Added HTTPS request to get steam's feature list

---
+++ app/src/main/java/com/example/gamezap/businessLogic/SteamFeature.java
@@ -0,0 +1,42 @@
+package com.example.gamezap.businessLogic;
+
+import java.util.List;
+
+public class SteamFeature {
+    private String id;
+    private String name;
+    private List<Game> games;
+
+    public SteamFeature() {
+    }
+
+    public SteamFeature(String id, String name, List<Game> games) {
+        this.id = id;
+        this.name = name;
+        this.games = games;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(List<Game> games) {
+        this.games = games;
+    }
+}
